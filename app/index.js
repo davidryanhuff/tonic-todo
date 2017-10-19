84bc7d333b799166ed49f2c2b8b95a0f05c36401
David Huff <davidryanhuff@gmail.com>
2017-10-19 06:44:33 -0600

    prefer const to let

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ import { reducer as todoApp } from './reducers'
 import Root from './components/Root'
 import './styles/main.scss'
 
-let store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
   
 // Create app DOM node.
 const appNode = document.createElement('div')
